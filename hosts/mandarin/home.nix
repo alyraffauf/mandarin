6c6d0150185b36c26bf5226fa6759332653cc1a3
Aly Raffauf <aly@raffauflabs.com>
2024-07-15 07:42:53 -0400

    add second monitor

--- hosts/mandarin/home.nix
+++ hosts/mandarin/home.nix
@@ -9,7 +9,10 @@
       {
         ar.home.desktop.hyprland = {
           autoSuspend = false;
-          monitors = ["desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,2"];
+          monitors = [
+            "desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,2"
+            "desc:LG Electronics LG IPS QHD 207NTVSE5615,preferred,-1152x0,1.25,transform,1"
+          ];
         };
       }
     ];
