a74b334079c50025c5e4891730c0143d41305b3e
Aly Raffauf <aly@raffauflabs.com>
2024-07-30 21:46:05 -0400

    home: conform to upstream autoSuspend syntax

--- hosts/mandarin/home.nix
+++ hosts/mandarin/home.nix
@@ -7,12 +7,14 @@
   home-manager = {
     sharedModules = [
       {
-        ar.home.desktop.hyprland = {
+        ar.home.desktop = {
           autoSuspend = false;
-          monitors = [
-            "desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,2"
-            "desc:LG Electronics LG IPS QHD 207NTVSE5615,preferred,-1152x0,1.25,transform,1"
-          ];
+          hyprland = {
+            monitors = [
+              "desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,2"
+              "desc:LG Electronics LG IPS QHD 207NTVSE5615,preferred,-1152x0,1.25,transform,1"
+            ];
+          };
         };
       }
     ];
