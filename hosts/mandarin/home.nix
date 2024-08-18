56bf66b8ae50af15edac4234b4a56fdd661f799f
Aly Raffauf <aly@raffauflabs.com>
2024-08-18 15:44:06 -0400

    hyprland: disable vrr

--- hosts/mandarin/home.nix
+++ hosts/mandarin/home.nix
@@ -11,7 +11,7 @@
           autoSuspend = false;
           hyprland = {
             monitors = [
-              "desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,2"
+              "desc:LG Electronics LG ULTRAWIDE 207NTHM9F673,preferred,auto,1.25,vrr,0"
               "desc:LG Electronics LG IPS QHD 207NTVSE5615,preferred,-1152x0,1.25,transform,1"
             ];
           };
